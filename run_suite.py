c0f778a9519c62049c1ff4e4e9e3c9dd0ec80f42
chuyifei917 <1015297461@qq.com>
2020-06-11 13:03:56 +0800

    demo_01

--- run_suite.py
+++ run_suite.py
@@ -1,10 +1,11 @@
 # 1.导包
 import unittest
 from BeautifulReport import BeautifulReport
+from app import BASE_DIR
 
 # 2.组织测试套件
 
-suite = unittest.TestLoader().discover("script", "test*.py")
+suite = unittest.TestLoader().discover(BASE_DIR+"/script", "test*.py")
 
 # 3.定义测试报告
 # report_file = "{}-report.html". format(time.strftime("%Y%m%d%H%M%S"))
@@ -12,5 +13,5 @@ suite = unittest.TestLoader().discover("script", "test*.py")
 
 # 使用beautiful批量
 file = "report.html"
-BeautifulReport(suite).report(filename=file, description="TpshopAPI", report_dir="report")
+BeautifulReport(suite).report(filename=file, description="TpshopAPI", report_dir=BASE_DIR+"/report")
 
